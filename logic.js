07d6b7b77fd19329e753706f0bea697f11454604
satender kumar <skstatenderkumar59@gmail.com>
2025-04-04 11:43:01 +0530

    reverse string sentence

---
+++ logic.js
@@ -0,0 +1,8 @@
+// revece the given string
+//-->ramesh hamra bhai hai
+
+function reverceString(str){
+    const updatedStr = str.split(" ").reverse().join(" ")
+    return updatedStr 
+}
+console.log(reverceString("hello ram")) 
