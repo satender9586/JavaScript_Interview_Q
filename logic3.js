076a50fa43b1d24542a4cdf001410cf00d47f13a
satender kumar <skstatenderkumar59@gmail.com>
2025-04-04 12:23:12 +0530

    Array : different way to create array

---
+++ logic3.js
@@ -0,0 +1,18 @@
+// array clone
+
+let second = Array.from("125478563")
+let first = [1,2,3,4,5,6,7,8,9]
+let third = new Array()
+third.push(1)
+third.push(2)
+third.push(3)
+third.push(4)
+third.push(5)
+let forth = Array.of(1,2,3,4,5,5)
+let five = [1,1,2,3,...first]
+let six = [58,56].concat(second)
+let sevem = []
+sevem.push(8)
+
+console.log(sevem)
+
