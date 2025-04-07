3082ca035748c4305e8ec1bd2de9b75451287ffb
satender kumar <skstatenderkumar59@gmail.com>
2025-04-07 12:20:08 +0530

    string : combo string palindrome

---
+++ logic7.js
@@ -0,0 +1,14 @@
+// check combo string palindrome
+
+function checkComboPalindrome(str) {
+  const spancesRemove = str.replace(/\s+/g, "").toLowerCase();
+  const revsers = spancesRemove.split("").reverse().join("");
+
+  if (spancesRemove === revsers) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkComboPalindrome("ollo "));
